548ef09d46fa6e394f1287ffedba1d984a1d6d3f
Jos3GS <gutierrez.sossa.j@gmail.com>
2024-07-14 16:54:50 -0500

    primer commit

---
+++ app/src/main/java/com/example/musicapp/Login.kt
@@ -0,0 +1,36 @@
+package com.example.musicapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class Login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_login)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val bottomNavigationView : BottomNavigationView = findViewById(R.id.bottomNavigationView_Login)
+        bottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId){
+                R.id.btn_navigation_login_login -> {
+
+                    true
+                }
+                R.id.btn_navigation_login_register -> {
+
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+}
