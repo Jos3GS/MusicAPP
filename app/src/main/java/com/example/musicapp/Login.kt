e97ff7ac1e2f1f14dfb5953d6cc8d2e4a659544f
Jos3GS <gutierrez.sossa.j@gmail.com>
2024-07-14 19:05:21 -0500

    Diseño de los 3 fragments de login, registro y About listos con cambio de fragment

--- app/src/main/java/com/example/musicapp/Login.kt
+++ app/src/main/java/com/example/musicapp/Login.kt
@@ -5,7 +5,9 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.musicapp.fragments.fragment_About
 import com.example.musicapp.fragments.fragment_login
+import com.example.musicapp.fragments.fragment_registro
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class Login : AppCompatActivity() {
@@ -19,20 +21,31 @@ class Login : AppCompatActivity() {
             insets
         }
 
+        if(savedInstanceState == null){
+            supportFragmentManager.beginTransaction()
+                .add(R.id.fragmentContainerLogin, fragment_login::class.java, null)
+                .commit()
+        }
+
         val bottomNavigationView : BottomNavigationView = findViewById(R.id.bottomNavigationView_Login)
         bottomNavigationView.setOnItemSelectedListener { item ->
             when (item.itemId){
                 R.id.btn_navigation_login_login -> {
                     supportFragmentManager.beginTransaction()
-                        .add(R.id.fragmentContainerLogin, fragment_login::class.java, null)
+                        .replace(R.id.fragmentContainerLogin, fragment_login::class.java, null)
                         .commit()
                     true
                 }
                 R.id.btn_navigation_login_register -> {
-
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.fragmentContainerLogin, fragment_registro::class.java, null)
+                        .commit()
                     true
                 }
                 R.id.btn_navigation_login_help -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.fragmentContainerLogin, fragment_About::class.java, null)
+                        .commit()
                     true
                 }
                 else -> false
